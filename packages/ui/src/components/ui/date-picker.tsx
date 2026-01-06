fc9b3c46c8d0e65f16be84e89460fa089639db5e
Austin-Rowe-NaN <Austin.Rowe.NaN@gmail.com>
2026-01-06 15:02:44 -0600

    Make date picker UX better.

--- packages/ui/src/components/ui/date-picker.tsx
+++ packages/ui/src/components/ui/date-picker.tsx
@@ -1,6 +1,10 @@
-import * as React from "react";
-import {DateTime} from 'luxon';
-import { Calendar as CalendarIcon } from "lucide-react";
+import { useState } from "react";
+import { DateTime } from "luxon";
+import {
+  Calendar as CalendarIcon,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
@@ -10,20 +14,49 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-export function DatePicker({defaultDate}: {defaultDate?: Date}) {
-  const [date, setDate] = React.useState<Date | undefined>(defaultDate);
+export function DatePicker({ defaultDate }: { defaultDate?: Date }) {
+  const [date, setDate] = useState<Date | undefined>(defaultDate);
 
   return (
     <Popover>
-      <PopoverTrigger asChild>
+      <div className="flex flex-row gap-1 items-center">
         <Button
-          variant="default"
-          data-empty={!date}
+          variant="ghost"
+          size="icon-sm"
+          onClick={() =>
+            setDate((prev) =>
+              prev
+                ? DateTime.fromJSDate(prev).minus({ days: 1 }).toJSDate()
+                : prev
+            )
+          }
         >
-          <CalendarIcon />
-          {date ? DateTime.fromJSDate(date).toFormat('MM/dd/yyyy') : <span>Pick a date</span>}
+          <ChevronLeft />
         </Button>
-      </PopoverTrigger>
+        <PopoverTrigger asChild>
+          <Button variant="default" data-empty={!date}>
+            <CalendarIcon />
+            {date ? (
+              DateTime.fromJSDate(date).toFormat("MM/dd/yyyy")
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <Button
+          variant="ghost"
+          size="icon-sm"
+          onClick={() =>
+            setDate((prev) =>
+              prev
+                ? DateTime.fromJSDate(prev).plus({ days: 1 }).toJSDate()
+                : prev
+            )
+          }
+        >
+          <ChevronRight />
+        </Button>
+      </div>
       <PopoverContent className="w-auto p-0">
         <Calendar mode="single" selected={date} onSelect={setDate} />
       </PopoverContent>
