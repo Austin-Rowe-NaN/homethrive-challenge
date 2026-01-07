d3e45236e09a28cec78a986b295d1bdbfc4ddce9
Austin-Rowe-NaN <Austin.Rowe.NaN@gmail.com>
2026-01-06 22:56:44 -0600

    Tweak API for better type support on the frontend and allow CORS.

--- packages/api/src/trpc/appRouter.ts
+++ packages/api/src/trpc/appRouter.ts
@@ -1,61 +1,65 @@
-import {initTRPC} from '@trpc/server';
+import { initTRPC } from "@trpc/server";
 import {
-    zodAddCompletedDoseInput,
-    MedicationModel,
-    zodMedication,
-    zodCareRecipientId
+  zodAddCompletedDoseInput,
+  MedicationModel,
+  zodMedication,
+  zodCareRecipientId,
 } from "@homethrive-challenge/api/schemas";
 
 export const t = initTRPC.create();
 
 export const appRouter = t.router({
-    getCareRecipientMedicationsById: t.procedure.input(zodCareRecipientId).query(async (opts) => {
-        try {
-            const { input: careRecipientId } = opts;
-            return MedicationModel.find({careRecipientId})
-        } catch (e){
-            console.error(e);
-            throw new Error('Failed to fetch medications');
-        }
+  getCareRecipientMedicationsById: t.procedure
+    .input(zodCareRecipientId)
+    .query(async (opts) => {
+      try {
+        const { input: careRecipientId } = opts;
+        return MedicationModel.find({ careRecipientId }).lean();
+      } catch (e) {
+        console.error(e);
+        throw new Error("Failed to fetch medications");
+      }
     }),
-    createMedication: t.procedure.input(zodMedication).mutation(async (opts) => {
-        try {
-            const { input } = opts;
-            return MedicationModel.create(input);
-        } catch (e){
-            console.error(e);
-            throw new Error('Failed to create medication');
-        }
+  createMedication: t.procedure.input(zodMedication).mutation(async (opts) => {
+    try {
+      const { input } = opts;
+      const newMedication = await MedicationModel.create(input);
+      return newMedication.toObject();
+    } catch (e) {
+      console.error(e);
+      throw new Error("Failed to create medication");
+    }
+  }),
+  addCompletedDose: t.procedure
+    .input(zodAddCompletedDoseInput)
+    .mutation(async (opts) => {
+      try {
+        const { input } = opts;
+        const { medicationId, doseDate } = input;
+        // Add doseDate, ensure uniqueness and ascending order
+        return MedicationModel.findByIdAndUpdate(
+          medicationId,
+          [
+            {
+              $set: {
+                completedDoses: {
+                  $sortArray: {
+                    input: {
+                      $setUnion: ["$completedDoses", [doseDate]],
+                    },
+                    sortBy: 1,
+                  },
+                },
+              },
+            },
+          ],
+          { new: true, updatePipeline: true, lean: true }
+        );
+      } catch (e) {
+        console.error(e);
+        throw new Error("Failed to add completed dose");
+      }
     }),
-    addCompletedDose: t.procedure.input(zodAddCompletedDoseInput).mutation(async (opts) => {
-        try{
-            const { input } = opts;
-            const { medicationId, doseDate } = input;
-            // Add doseDate, ensure uniqueness and ascending order
-            return MedicationModel.findByIdAndUpdate(
-                medicationId,
-                [
-                    {
-                        $set: {
-                            completedDoses: {
-                                $sortArray: {
-                                    input: {
-                                        $setUnion: ["$completedDoses", [doseDate]]
-                                    },
-                                    sortBy: 1
-                                }
-                            }
-                        }
-                    }
-                ],
-                { new: true, updatePipeline: true }
-            );
-        }
-        catch(e){
-            console.error(e);
-            throw new Error('Failed to add completed dose');
-        }
-    })
 });
 
 export type AppRouter = typeof appRouter;
