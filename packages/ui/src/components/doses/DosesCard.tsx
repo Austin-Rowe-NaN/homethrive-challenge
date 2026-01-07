ab65c820c87f738c806b57c89312f712e6b34c7a
Austin-Rowe-NaN <Austin.Rowe.NaN@gmail.com>
2026-01-07 02:15:49 -0600

    Add mutation for marking doses as taken and show spinner during processing.

--- packages/ui/src/components/doses/DosesCard.tsx
+++ packages/ui/src/components/doses/DosesCard.tsx
@@ -10,16 +10,19 @@ import {
   ItemTitle,
 } from "@/components/ui/item.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { trpc } from "@/utils/trpc.ts";
 import { DateTime } from "luxon";
-import {useMemo, useState} from "react";
+import { useMemo, useState } from "react";
 import { formatHour } from "@/utils/format.ts";
 import { Skeleton } from "@/components/ui/skeleton.tsx";
-import {Check} from "lucide-react";
+import { Check } from "lucide-react";
+import { Spinner } from "@/components/ui/spinner.tsx";
 
 export default function DosesCard() {
-    const [doseScheduleDate, setDoseScheduleDate] = useState<Date | undefined>(new Date());
+  const [doseScheduleDate, setDoseScheduleDate] = useState<Date | undefined>(
+    new Date()
+  );
   const {
     isFetching: isFetchingMedications,
     data: medications,
@@ -34,7 +37,8 @@ export default function DosesCard() {
     isFetching: isFetchingDoses,
     data: doses,
     error: dosesFetchError,
-  } = useQuery( // TODO - fix cache behavior when changing date
+  } = useQuery(
+    // TODO - fix cache behavior when changing date
     trpc.getCareRecipientDosesForGivenDate.queryOptions(
       {
         careRecipientId: 2193,
@@ -43,10 +47,22 @@ export default function DosesCard() {
       {
         refetchOnWindowFocus: false,
         retry: 3,
-        enabled: doseScheduleDate !== undefined
+        enabled: doseScheduleDate !== undefined,
       }
     )
   );
+  const queryClient = useQueryClient();
+  const dosesQueryKey = trpc.getCareRecipientDosesForGivenDate.queryKey();
+  const medicationsQueryKey = trpc.getCareRecipientMedicationsById.queryKey();
+  const { mutate, isPending: addCompletedDosePending } = useMutation(
+    trpc.addCompletedDose.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: dosesQueryKey });
+        queryClient.invalidateQueries({ queryKey: medicationsQueryKey });
+      },
+    })
+  );
+
   const fetching = isFetchingMedications || isFetchingDoses;
   const doseObjects = useMemo(() => {
     if (
@@ -68,7 +84,7 @@ export default function DosesCard() {
           medication,
         };
       })
-      .filter(Boolean) as unknown as [
+      .filter(Boolean).sort((a, b) => a!.medication.schedule.timeOfDay - b!.medication.schedule.timeOfDay) as unknown as [
       (typeof doses)[0] & { medication: (typeof medications)[0] }
     ];
   }, [doses, medications, fetching]);
@@ -78,7 +94,7 @@ export default function DosesCard() {
       cardHeader={
         <CardHeader className="flex justify-between flex-wrap @max-lg/MedicationsCard:justify-center">
           <CardTitle className="text-2xl">Dosage Schedule</CardTitle>
-          <DatePicker date={doseScheduleDate} setDate={setDoseScheduleDate}/>
+          <DatePicker date={doseScheduleDate} setDate={setDoseScheduleDate} />
         </CardHeader>
       }
       cardContent={
@@ -105,11 +121,25 @@ export default function DosesCard() {
                         </ItemDescription>
                       </ItemContent>
                       <ItemActions>
-                          {doseObject.completed? (
-                            <Button className="bg-chart-5 hover:bg-chart-5"><Check /> Taken</Button>
-                          ) : (
-                              <Button>Mark as taken</Button>
-                          )}
+                        {doseObject.completed ? (
+                          <Button className="bg-chart-5 hover:bg-chart-5">
+                            <Check /> Taken
+                          </Button>
+                        ) : (
+                          <Button
+                            onClick={() =>
+                              mutate({
+                                medicationId: doseObject.medication._id,
+                                doseDate: DateTime.fromJSDate(doseScheduleDate!)
+                                  .startOf("day")
+                                  .toISO(),
+                              })
+                            }
+                          >
+                            Mark as taken{" "}
+                            {addCompletedDosePending && <Spinner />} {/* TODO - make this only show on the dose we're mutating*/}
+                          </Button>
+                        )}
                       </ItemActions>
                     </Item>
                   ))
